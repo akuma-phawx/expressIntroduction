7b1e01d5728b2a71a072753c27bdfeece7d7925d
akuma-phawx <chrisvradis00@gmail.com>
2020-11-06 11:55:53 +0200

    Routing

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 const express = require("express");
 const app = express();
+const log = console.log;
+const dir = console.dir;
 
 //all requests delt with use
-app.use(() => {
-  console.log("we got your request");
-});
+// app.use((req, res) => {
+//   log("Request received.");
+// });
 
 //localhost: 3030
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
+app.listen(8080, () => {
+  log("Server starting.. Listening on port 8080.");
+});
+
+// Path: /cats  => 'meow'
+app.get("/cats", (req, res) => {
+  log("Cats request");
+  res.send("Meow!");
+});
+// Path: /dogs  => 'woof'
+app.get("/dogs", (req, res) => {
+  log("Dogs request");
+  res.send("Woof!");
+});
+// Path: /  => 'Willkommen'
+app.get("/", (req, res) => {
+  log("Home request");
+  res.send("Willkommen!");
 });
